da6ff2231a68ab694d91afdac54e6b0d759109f0
Jiahao Li <isundaylee.reg@gmail.com>
2015-11-16 16:40:35 -0500

    Adds sounds.

---
+++ index.js
@@ -0,0 +1,37 @@
+$(function() {
+    var play_sound = function(bite) {
+        bite.find('i.fa-pause').show();
+        bite.find('i.fa-play').hide();
+        bite.find('#player').addClass('active')
+
+        if (window.current_cell) {
+            stop_sound(window.current_cell);
+            window.sound.pause();
+        }
+
+        window.current_cell = bite
+        window.sound = new Audio("sounds/" + bite.data('src'));
+        window.sound.onended = function() {
+            stop_sound(window.current_cell)
+            window.current_cell = null
+            window.sound = null
+        }
+        window.sound.play();
+    }
+
+    var stop_sound = function(bite) {
+        window.sound.pause();
+
+        bite.find('i.fa-pause').hide();
+        bite.find('i.fa-play').show();
+        bite.find('#player').removeClass('active')
+    }
+
+    $('.soundbite i.fa-play').click(function() {
+        play_sound($(this).parents('.soundbite'));
+    })
+
+    $('.soundbite i.fa-pause').click(function() {
+        stop_sound($(this).parents('.soundbite'));
+    })
+})
